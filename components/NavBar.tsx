3cbcab2a6d478a624833a82bec2c8f3e1daff329
RubenF01 <rubenfrias10@gmail.com>
2023-01-21 20:32:56 -0400

    implemented basic layout for nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,25 @@
+import SearchIcon from "../public/icons/magnifying-glass-solid.svg";
+import Image from "next/image";
+import TransparentLogo from "../public/ASAP NEWS-logos_transparent.png";
+import Link from "next/link";
+
 const NavBar = () => {
   return (
-    <div>
-      <div></div>
+    <div className="relative flex justify-center py-3">
+      <SearchIcon className="absolute top-0 bottom-0 w-5 pt-3 my-auto left-10" />
+      <div className="flex items-center gap-x-5">
+        <nav className="flex uppercase gap-x-5">
+          <Link href="/">business</Link>
+          <Link href="/">entertainment</Link>
+          <Link href="/">politics</Link>
+        </nav>
+        <Image src={TransparentLogo} alt="logo" className="w-44" />
+        <nav className="flex uppercase gap-x-5">
+          <Link href="/">science</Link>
+          <Link href="/">technology</Link>
+          <Link href="/">world</Link>
+        </nav>
+      </div>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "@/components/Layout";
+import { Lato } from "@next/font/google";
+
+const lato = Lato({ subsets: ["latin"], weight: ["700", "400", "300", "100"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={lato.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
